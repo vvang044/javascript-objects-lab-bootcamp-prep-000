e53566fbf139cc2f289a64ee5b784fcc3350d3a6
Vik Vangala <vvang044@gmail.com>
2019-05-28 19:09:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function deleteFromObjectByKey(object,key){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key , value){
-  object.key = value;
+  object[key] = value;
   return object
 }
