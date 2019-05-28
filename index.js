fcbf1beb89fc16ab76778d516975d7a34e8b8062
Vik Vangala <vvang044@gmail.com>
2019-05-28 19:14:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key , value){
   return object
 }
 
+function updateObjectWithKeyandValue(object, key, value){
+  var newObj = Object.assign({}, object)
+  
+}
